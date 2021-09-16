a8d9627833292d9707153dee9c1633a72fe5f411
milos018 <milos018@gmail.com>
2021-09-16 14:53:17 +0200

    update API URL

--- src/homeRoute/home-index.vue
+++ src/homeRoute/home-index.vue
@@ -1,33 +1,37 @@
 <template>
-	<ul v-if="!loading" class="flex justify-center flex-wrap gap-3">
-		<movie-item
-			v-for="movie in movies"
-			:key="movie._id"
-			:movie="movie"
-		></movie-item>
-	</ul>
+  <ul v-if="!loading" class="flex justify-center flex-wrap gap-3">
+    <movie-item
+      v-for="movie in movies"
+      :key="movie._id"
+      :movie="movie"
+    ></movie-item>
+  </ul>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import axios from 'axios';
-import MovieItem from './components/movie-item.vue';
+import { ref } from "vue";
+import axios from "axios";
+import MovieItem from "./components/movie-item.vue";
 
 const loading = ref(false);
 const movies = ref([]);
 
 const getAllMovies = async () => {
-	loading.value = true;
-	let movieList;
-	try {
-		const res = await axios.get('http://localhost:5500/movies');
-		movieList = res.data;
-		loading.value = false;
-		movies.value = movieList;
-	} catch (error) {
-		loading.value = false;
-		console.log(error);
-	}
+  loading.value = true;
+  let movieList;
+  try {
+    const res = await axios.get(
+      import.meta.env.PROD
+        ? "https://mevn-mflix-fcc.herokuapp.com/movies"
+        : "http://localhost:5500/movies"
+    );
+    movieList = res.data;
+    loading.value = false;
+    movies.value = movieList;
+  } catch (error) {
+    loading.value = false;
+    console.log(error);
+  }
 };
 
 getAllMovies();
