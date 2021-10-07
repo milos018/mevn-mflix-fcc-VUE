3b34930b0653810b1c599a6d435cefe87837fcb2
Milos Dimitrijevic <milos018@gmail.com>
2021-10-07 10:56:28 +0200

    component rename

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <the-header></the-header>
-  <the-search-filter v-if="shouldShowHeader"></the-search-filter>
-  <main bg="light-blue-100" p="2" class="max-w-6xl mx-auto">
-    <router-view></router-view>
-  </main>
+	<the-header />
+	<the-search-filter v-if="shouldShowHeader" />
+	<main bg="light-blue-100" p="2" class="max-w-6xl mx-auto">
+		<router-view />
+	</main>
 </template>
 
 <script setup>
-import { computed } from "@vue/reactivity";
-import { useRoute } from "vue-router";
+import { computed } from '@vue/reactivity';
+import { useRoute } from 'vue-router';
 
-import TheHeader from "./_nav/the-header.vue";
-import TheSearchFilter from "./_nav/the-search-filter.vue";
+import TheHeader from './_nav/the-header.vue';
+import TheSearchFilter from './_nav/the-search-filter.vue';
 
-import { updateUser } from "./hooks/useAuth";
+import { updateUser, userToken } from './hooks/useAuth';
 
-if (JSON.parse(localStorage.getItem("user"))) {
-  updateUser(JSON.parse(localStorage.getItem("user")));
+if (JSON.parse(localStorage.getItem('user'))) {
+	updateUser(JSON.parse(localStorage.getItem('user')));
 }
 
 const route = useRoute();
-const shouldShowHeader = computed(() => route.fullPath !== "/auth");
+const shouldShowHeader = computed(() => route.path !== '/auth');
 </script>
 
 <style>
 #logo-text {
-  text-decoration: none;
+	text-decoration: none;
 }
 
 a.router-link-exact-active {
-  text-decoration: underline;
+	text-decoration: underline;
 }
 </style>
